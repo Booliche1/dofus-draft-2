2ddbca0f8a28ba8a8e0dd36a8f484b05d63c33b7
Booliche1 <Boolichemenalt1@gmail.com>
2024-06-25 18:30:23 +0200

    Fix

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,29 @@
 </template>
 
 <script>
-import DraftModule from './components/DraftModule.vue';
+import DraftModule from './components/DraftModule.vue'
 
 export default {
   name: 'App',
   components: {
     DraftModule
   }
-};
+}
 </script>
 
 <style>
-/* Ton style global */
+/* Ajoutez ici les styles globaux si nécessaire */
+body, html {
+  font-family: 'Inter', Arial, sans-serif;
+  background-color: #121212;
+  color: #ffffff;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
